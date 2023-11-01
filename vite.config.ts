23f683c2017c40b4d58f3a601adbb21d8fe8f84d
Alen Subasic <alensubasic@gmail.com>
2023-11-01 10:22:45 -0500

    publishPath

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 })
