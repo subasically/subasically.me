0a5f58ffe8beb81688c918fdfc35ef36d8e43bcf
Alen Subasic <alensubasic@gmail.com>
2023-11-03 09:45:02 -0500

    clean up

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ const locales: any = {
 
 <template>
   <header class="flex center-horizontal center-vertical">
-    <img src="./assets/me.png" class="me" alt="Alen Subasic" />
+    <img src="./assets/me.webp" class="me" alt="Alen Subasic" />
   </header>
   <main class="flex column center-vertical center-horizontal">
     <h1>Alen Subasic</h1>
@@ -42,7 +42,7 @@ const locales: any = {
   <footer id="footer" class="flex column center-vertical center-horizontal">
     <ul>
       <li>
-        <img :src="locales[browserLang]['flag']" alt="flag" class="flag">
+        <img :src="locales[browserLang]['flag']" alt="flag" class="footer flag logo">
       </li>
       <li>
         <a href="https://github.com/subasically" alt="github" target="_blank">
@@ -51,7 +51,15 @@ const locales: any = {
       </li>
       <li>
         <a href="https://www.linkedin.com/in/alensubasic" alt="linkedin" target="_blank">
-          <img src="./assets/linkedin.svg" alt="linkedin logo" class="footer logo">
+          <svg xmlns="http://www.w3.org/2000/svg" class="footer logo linkedin" width="24" height="24" viewBox="0 0 24 24">
+            <path
+              d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="#resume">
+          <img src="./assets/cv-4.svg" class="footer logo">
         </a>
       </li>
       <li>
