7cff4f3273c443966cb0b5141dd7b5737eec7350
Alen Subasic <alensubasic@gmail.com>
2026-03-13 10:17:11 -0500

    Fix Plausible script loading

--- plugins/plausible.client.ts
+++ plugins/plausible.client.ts
@@ -7,34 +7,25 @@ export default defineNuxtPlugin(() => {
     return;
   }
 
-  if (plausibleScriptSrc) {
-    useHead({
-      script: [
-        {
-          async: true,
-          src: plausibleScriptSrc,
-        },
-        {
-          innerHTML:
-            "window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()",
-        },
-      ],
-    });
+  const scriptSrc = plausibleScriptSrc || (plausibleDomain
+    ? `${plausibleApiHost.replace(/\/$/, "")}/js/script.js`
+    : '');
 
-    return;
-  }
-
-  if (!plausibleDomain) {
+  if (!scriptSrc) {
     return;
   }
 
   useHead({
     script: [
       {
-        defer: true,
-        "data-domain": plausibleDomain,
-        src: `${plausibleApiHost.replace(/\/$/, "")}/js/script.js`,
+        innerHTML:
+          "window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()",
       },
+      {
+        defer: true,
+        ...(plausibleDomain ? { "data-domain": plausibleDomain } : {}),
+        src: scriptSrc,
+      }
     ],
   });
 });
